afcd4da1de70e7d5055b96ee1e236f2b805651cf
Lala Patle <Lala Patle@LAPTOP-KBF3AQHL>
2020-12-22 16:56:29 +0530

    lombok

--- src/main/java/com/capgemini/tlta/model/LearningActivity.java
+++ src/main/java/com/capgemini/tlta/model/LearningActivity.java
@@ -21,9 +21,16 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 @Entity
 @Table(name="learning_activity")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class LearningActivity implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
@@ -52,87 +59,5 @@ public class LearningActivity implements Serializable{
 	@JoinColumn(name="assesment_id")
 	private Assessment assesment;
 	
-	@OneToMany(mappedBy = "learningActivity",cascade = CascadeType.ALL)
-	private Set<UserActivity> userActivity = new HashSet<UserActivity>();
 	
-	public Set<UserActivity> getUserActivity(){
-		return userActivity;
-	}
-	
-	public void setUserActivity(Set<UserActivity> groups) {
-		this.userActivity = groups;
-	}
-	
-	public void addUserActivity(UserActivity userActivity) {
-		this.userActivity.add(userActivity);
-	}
-
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public String getActivity_name() {
-		return activity_name;
-	}
-	public void setActivity_name(String activity_name) {
-		this.activity_name = activity_name;
-	}
-	public String getActivity_link() {
-		return activity_link;
-	}
-	public void setActivity_link(String activity_link) {
-		this.activity_link = activity_link;
-	}
-	public String getActivity_level() {
-		return activity_level;
-	}
-	public void setActivity_level(String activity_level) {
-		this.activity_level = activity_level;
-	}
-	public Double getActivity_time() {
-		return activity_time;
-	}
-	public void setActivity_time(Double activity_time) {
-		this.activity_time = activity_time;
-	}
-	public Date getActivity_realsedate() {
-		return activity_realsedate;
-	}
-	public void setActivity_realsedate(Date activity_realsedate) {
-		this.activity_realsedate = activity_realsedate;
-	}
-	public Assessment getAssesment() {
-		return assesment;
-	}
-	public void setAssesment(Assessment assesment) {
-		this.assesment = assesment;
-	}
-	
-	//No argument constructor
-	public LearningActivity() {
-		
-	}
-	
-	//All arguments constructor
-	public LearningActivity(Integer id, String activity_name, String activity_link, String activity_level, Double activity_time,
-			Date activity_realsedate, Assessment assesment) {
-		super();
-		this.id = id;
-		this.activity_name = activity_name;
-		this.activity_link = activity_link;
-		this.activity_level = activity_level;
-		this.activity_time = activity_time;
-		this.activity_realsedate = activity_realsedate;
-		this.assesment = assesment;
-	}
-	
-	// toString method
-	@Override
-	public String toString() {
-		return "Activity [id=" + id + ", activity_name=" + activity_name + ", activity_link=" + activity_link
-				+ ", activity_level=" + activity_level + ", activity_time=" + activity_time + ", activity_realsedate="
-				+ activity_realsedate + ", assesment=" + assesment + "]";
-	}
 }
