6d7695844f0209d00e5762c39ea27dcec027c496
user <user@DESKTOP-0QHGN6T>
2020-12-23 15:18:13 +0530

    LearningController and its service layer

--- src/main/java/com/capgemini/tlta/sevice/LearningActivityServiceImpl.java
+++ src/main/java/com/capgemini/tlta/sevice/LearningActivityServiceImpl.java
@@ -1,47 +1,99 @@
 package com.capgemini.tlta.sevice;
 
 import java.util.List;
-
-
+import java.util.Optional;
 import javax.persistence.PersistenceException;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import com.capgemini.tlta.exception.ActivityException;
-import com.capgemini.tlta.exception.AssesmentException;
+import com.capgemini.tlta.model.Assessment;
 import com.capgemini.tlta.model.LearningActivity;
+import com.capgemini.tlta.repository.AssessmentActivityRepository;
 import com.capgemini.tlta.repository.LearningActivityRepository;
-import com.capgemini.tlta.repository.LearningActivityDaoImpl;
 
+@Service( value = "learningActivityService")
+@Transactional
 public class LearningActivityServiceImpl implements LearningActivityService{
 
+	@Autowired
+	LearningActivityRepository learningActivityRepository;
+//	@Autowired
+//	AssessmentActivityRepository assessmentActivityRepository;
 	@Override
-	public Integer addLearningActivity(LearningActivity learningActivity)
-			throws PersistenceException, AssesmentException, ActivityException {
-		// TODO Auto-generated method stub
-		return null;
+	public LearningActivity addLearningActivity(LearningActivity learningActivity)
+			throws PersistenceException,ActivityException {
+
+		try {
+			LearningActivity learning = new LearningActivity();
+			learning = learningActivityRepository.save(learningActivity);
+			return learning;
+		}catch(DataAccessException e) {
+			throw new ActivityException(e.getMessage(),e);
+		}catch(Exception e) {
+			throw new  ActivityException(e.getMessage(),e);
+		}
 	}
 
 	@Override
 	public LearningActivity searchLearningActivityById(Integer id) throws ActivityException {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			Optional<LearningActivity> optional= 
+					learningActivityRepository.findById(id);
+			if(optional.isPresent()) {
+				System.out.println(optional.get());
+				return optional.get();
+			}else {
+				return null;
+			}			
+		}catch(DataAccessException e) {
+			throw new ActivityException(e.getMessage(),e);
+		}catch(Exception e) {
+			throw new  ActivityException(e.getMessage(),e);
+		}
 	}
 
 	@Override
 	public Integer deleteLearningActivity(Integer id) throws ActivityException {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			learningActivityRepository.deleteById(id);
+			return 1;
+		}catch(DataAccessException e) {
+			throw new ActivityException(e.getMessage(),e);
+		}catch(Exception e) {
+			throw new ActivityException(e.getMessage(),e);
+		}
 	}
 
 	@Override
 	public List<LearningActivity> getAllLearningActivity() throws ActivityException {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			List<LearningActivity>learningList=
+					learningActivityRepository.findAll();
+			return learningList;
+		}catch(DataAccessException e) {
+			throw new ActivityException(e.getMessage(),e);
+		}catch(Exception e) {
+			throw new ActivityException(e.getMessage(),e);
+		}
 	}
-
+	@Autowired
+	AssessmentActivityRepository assessmentActivityRepository;
 	@Override
-	public LearningActivity updateLearningActivity(LearningActivity learningActivity) throws ActivityException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	public LearningActivity updateLearningActivity(LearningActivity learningActivity, Integer id) throws ActivityException {
+		Assessment assessment = null;
+		try {
+			assessment = assessmentActivityRepository.getOne(id);
+			learningActivity.setAssesment(assessment);
 
+			LearningActivity learningAct= 
+					learningActivityRepository.save(learningActivity);
+			return learningAct;
+	}catch(DataAccessException e) {
+		throw new ActivityException(e.getMessage(),e);
+	}catch(Exception e) {
+		throw new ActivityException(e.getMessage(),e);
+	}
+	}
 }
